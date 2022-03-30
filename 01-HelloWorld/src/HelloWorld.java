6bb838b45ae48ff12b4daab5d36e35105394c394
radiomanyi <pyzyf1983@gmail.com>
2022-03-30 16:43:08 +0800

    测试作业发布16:43

---
+++ 01-HelloWorld/src/HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Ye");
+		System.out.println("Use Internet to learn");
+		System.out.println("This is Test 1");
+	}
+
+}
---
+++ 02-Algorithm/src/Algorithm.java
@@ -0,0 +1,14 @@
+
+public class Algorithm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=1;
+		int b=2;
+		int c = a+b;
+		System.out.print("1+2=");
+		System.out.println(c);
+		System.out.println("This is Test 2");
+	}
+
+}
