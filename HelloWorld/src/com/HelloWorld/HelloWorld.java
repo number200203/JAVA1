240a0b61e710510f3461ec122683c7774ab00406
radiomanyi <pyzyf1983@gmail.com>
2022-03-30 16:22:06 +0800

    测试修改意见16:21

--- HelloWorld/src/com/HelloWorld/HelloWorld.java
+++ HelloWorld/src/com/HelloWorld/HelloWorld.java
@@ -3,8 +3,9 @@ package com.HelloWorld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		System.out.print("Hello Ye");
 		System.out.println("Use Internet to learn");
+		System.out.println("This is Test 1");
 	}
 }
