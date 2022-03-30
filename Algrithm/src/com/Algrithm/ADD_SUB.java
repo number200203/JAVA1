34ffddd09b292098ed645947b4640d25dce8b6e5
radiomanyi <pyzyf1983@gmail.com>
2022-03-30 16:33:38 +0800

    测试修改意见16:33

--- Algrithm/src/com/Algrithm/ADD_SUB.java
+++ Algrithm/src/com/Algrithm/ADD_SUB.java
@@ -3,12 +3,13 @@ package com.Algrithm;
 public class ADD_SUB {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		int a=1;
 		int b=2;
 		int c = a+b;
 		System.out.print("1+2=");
 		System.out.println(c);
+		System.out.println("This is Test 2");
 	}
 
 }
--- HelloWorld/src/com/HelloWorld/HelloWorld.java
+++ HelloWorld/src/com/HelloWorld/HelloWorld.java
@@ -1,10 +1,8 @@
-package com.HelloWorld;
-
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		
-		System.out.print("Hello Ye");
+		System.out.println("Hello Ye");
 		System.out.println("Use Internet to learn");
 		System.out.println("This is Test 1");
 	}
