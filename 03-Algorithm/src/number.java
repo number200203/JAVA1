fdd6b6bd921d63389a2ca942932cb45a907d2dc9
number200203 <102226379+number200203@users.noreply.github.com>
2022-04-05 19:43:17 +0800

    1

---
+++ 03-Algorithm/src/number.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Scanner in = new Scanner(System.in);
+	//	System.out.println("echo:"+ in.hasNextLine());
+	//	System.out.println("ÄãºÃ");
+		int a=1;
+		int b=3;
+		int c=a+b;
+		int d=b-c;
+		System.out.println("ÊäÈëÊı×Ö");
+	//	Scanner in = new Scanner(Scanner.in);
+		System.out.println("a+b="+c);
+		System.out.println("d="+d);
+	//	int price;
+	//	price=in.nextint();
+		
+		
+}
+}
